d94cfb6806728680b5e47645d4cd93fb23acf97e
Mei2Mei <angeline2311warindi.gmail.com>
2026-07-23 14:21:07 +0300

    modified index.jsa and axios.js

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -4,10 +4,6 @@ const BASE_URL = import.meta.env.VITE_API_URL || "";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  headers: {
-    'Cache-Control': 'no-cache',
-    'Pragma': 'no-cache',
-  },
 });
 
 api.interceptors.request.use((config) => {
